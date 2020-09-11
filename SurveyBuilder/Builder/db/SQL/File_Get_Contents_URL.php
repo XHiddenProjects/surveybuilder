84289c21a1fe523dc2b257c11910104327f46d8a
SurveyBuilder-Admin <64079569+SurveyBuilderAdmin@users.noreply.github.com>
2020-09-11 19:16:24 -0400

    Add files via upload

--- SurveyBuilder/Builder/db/SQL/File_Get_Contents_URL.php
+++ SurveyBuilder/Builder/db/SQL/File_Get_Contents_URL.php
@@ -2,9 +2,10 @@
 $title = $_POST['title-clone'];
 $file= $title . '.php';
 $content = file_get_contents($file);
+$letNewContent = htmlspecialchars($content);
 echo "Copy the text below and paste it into the textbox";
 echo "<br/>";
 echo "<br/>";
-echo $content;
+echo "<textarea style='margin: 0px; width: 1438px; height: 545px;'>" . $letNewContent . "</textarea>";
 header("EditDatabase.php");
 ?>
