ea1c5f2acdaa446571c38be9bc2dff32806a61ba
SurveyBuilder-Admin <64079569+SurveyBuilderAdmin@users.noreply.github.com>
2020-09-19 20:50:25 -0400

    Add files via upload

--- SurveyBuilder/Builder/SurveyBuilder.php
+++ SurveyBuilder/Builder/SurveyBuilder.php
@@ -9,7 +9,7 @@
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
  <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
- <script src="https://kit.fontawesome.com/46bb4793e2.js"></script>
+ <!--<script src="https://kit.fontawesome.com/46bb4793e2.js"></script>-->
 
 
 
@@ -26,15 +26,16 @@
  <meta name="UsersForm" hidden="hidden" content="SurveyBuilderId:19494sbybfdyfuy47grfrfureuyervchfiuerhgyuer748gryru435943578347f"/>
  <meta name="Forum" property="og:url" content="https://surveymaker.boards.net"/>
 <title id="UserTitle">Untitled - Survey Builder</title>
-<!--<link rel="stylesheet" href="./fontawesome/css/all.css"/>-->
+<link rel="stylesheet" href="./fontawesome/css/all.css"/>
 <link rel="stylesheet" type="text/css" href="SurveyBuilder.css"/>
 <link rel="stylesheet" href="SurveyBuilderMobile.css"/>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" type="text/css" href="FormInsert.css"/>
 <link rel="shortcut icon" href="favicon.ico"/>
+<link rel="stylesheet" href="https://proicons.netlify.app/css/icons.min.css"/>
 <script src="SurveyBuilder.js" type="text/javascript"></script>
 <script src="InsertItems.js" type="text/javascript"></script>
- <script src="https://kit.fontawesome.com/46bb4793e2.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/46bb4793e2.js" crossorigin="anonymous"></script>
 <script src="serviceworker.js"></script>
  <!--<link href="./app.webmanifest" rel="manifest" crossorigin="use-credentials"/>-->
 <noscript><img src="/SurveyBuilder/images/icon/favicon.png" width="20" height="20"/>Sorry JavaScript is off, make sure it is on due to a lot of functions needing to be triggered</noscript>
@@ -1289,15 +1290,15 @@ setTimeout(checkInternetConnection, 0);
 function checkInternetConnection(){
 let InternetStatus = window.navigator.onLine;
 if(InternetStatus){
-	let codeWifiConnected = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-wifi' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M15.385 6.115a.485.485 0 0 0-.048-.736A12.443 12.443 0 0 0 8 3 12.44 12.44 0 0 0 .663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c2.507 0 4.827.802 6.717 2.164.204.148.489.13.668-.049z'/><path d='M13.229 8.271c.216-.216.194-.578-.063-.745A9.456 9.456 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.577 1.336c.205.132.48.108.652-.065zm-2.183 2.183c.226-.226.185-.605-.1-.75A6.472 6.472 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.408.19.611.09A5.478 5.478 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.611-.091l.015-.015zM9.06 12.44c.196-.196.198-.52-.04-.66A1.99 1.99 0 0 0 8 11.5a1.99 1.99 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .708 0l.707-.707z'/></svg>";
-	document.querySelector(".Internet-Status").innerHTML = codeWifiConnected + "Connected";
+	let codeWifiConnected = "<i class='fas fa-wifi'></i>";
+	document.querySelector(".Internet-Status").innerHTML = codeWifiConnected + " Connected";
 	document.querySelector(".Internet-Status").style.fontSize = "32px";
 	document.querySelector(".Internet-Status").style.color = "green";
 	document.querySelector(".Internet-Status").style.textAlign = "center";
 	   document.querySelector("#Sidebar").hidden = false;
 }else{
-	let codeWifiDisconnected = "<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-wifi-off' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M10.706 3.294A12.545 12.545 0 0 0 8 3 12.44 12.44 0 0 0 .663 5.379a.485.485 0 0 0-.048.736.518.518 0 0 0 .668.05A11.448 11.448 0 0 1 8 4c.63 0 1.249.05 1.852.148l.854-.854zM8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065 8.448 8.448 0 0 1 3.51-1.27L8 6zm2.596 1.404l.785-.785c.63.24 1.228.545 1.785.907a.482.482 0 0 1 .063.745.525.525 0 0 1-.652.065 8.462 8.462 0 0 0-1.98-.932zM8 10l.934-.933a6.454 6.454 0 0 1 2.012.637c.285.145.326.524.1.75l-.015.015a.532.532 0 0 1-.611.09A5.478 5.478 0 0 0 8 10zm4.905-4.905l.747-.747c.59.3 1.153.645 1.685 1.03a.485.485 0 0 1 .048.737.518.518 0 0 1-.668.05 11.496 11.496 0 0 0-1.812-1.07zM9.02 11.78c.238.14.236.464.04.66l-.706.706a.5.5 0 0 1-.708 0l-.707-.707c-.195-.195-.197-.518.04-.66A1.99 1.99 0 0 1 8 11.5c.373 0 .722.102 1.02.28zm4.355-9.905a.53.53 0 1 1 .75.75l-10.75 10.75a.53.53 0 0 1-.75-.75l10.75-10.75z'/></svg>";
-	document.querySelector(".Internet-Status").innerHTML = codeWifiDisconnected + "Disconnected";
+	let codeWifiDisconnected = "<i class='fad fa-wifi-slash'></i>";
+	document.querySelector(".Internet-Status").innerHTML = codeWifiDisconnected + " Disconnected";
 	document.querySelector(".Internet-Status").style.fontSize = "32px";
 	document.querySelector(".Internet-Status").style.color = "red";
 	document.querySelector(".Internet-Status").style.textAlign = "center";
