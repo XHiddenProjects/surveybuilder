e1a80f9743d9e5ccb84f68606f1d75d43eedb0af
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2020-11-10 16:53:38 -0500

    Add files via upload

--- SurveyBuilder/Builder/API/GetAPI.php
+++ SurveyBuilder/Builder/API/GetAPI.php
@@ -1,17 +1,3 @@
 <?php
-echo "<head><title>getAPI - SurveyBuilder</title><link rel='shortcut icon' href='favicon.ico'/></head>";
-
-$char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-$gen = substr(str_shuffle($char),0,350);
-
-$file = "./API.txt";
-if(is_file($file)){
-echo "You already have this file";
-}
-else{
-$fileSet = fopen("$file", "w+");
-fwrite($fileSet, "$gen");
-fclose($fileSet);	
-echo "File was created";
-}
+echo "<script>window.open('http://surveybuilder.epizy.com/API/GetAPI.php')</script>";
 ?>
